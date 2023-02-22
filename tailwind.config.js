3ebb45cbd3626e58d04d8670ebb71f7f830ef63a
Hessen Nasser <hessenaloosh@gmail.com>
2023-02-22 14:02:53 +0200

    The END <3

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       'second-color': '#462E6A',
       'transparent-color': '#212a4a',
       'dark-color': '#3C567B',
+      'gray': '#b8b8b8',
+      'gradient1': '#6888C8',
+      'gradient2': '#5A98F2',
     },
     container: {
       center: true,
